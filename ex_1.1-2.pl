227ca94b7dbd40ac51ba6b61e9a62fa43b9062cc
Federico Mazzini <federico.mazzini3@studio.unibo.it>
2021-05-10 13:34:09 +0200

    refactor

---
+++ ex_1.1-2.pl
@@ -0,0 +1,15 @@
+% search(Elem, List)
+search(X, [X|_]). %search is OK if the element X is the head of the list
+search(X, [_|Xs]) :- search(X, Xs). %search is OK if the element X occurs in the tail Xs
+
+
+% search2(Elem, List)
+% looks for two consecutive occurrences of Elem
+search2(X, [X,X|_]).
+search2(X, [_|Xs]):- search2(X,Xs). % only change the base case, this case is the same
+
+% – search2(a,[b,c,a,a,d,e,a,a,g,h]). -> yes 2 solution?
+% – search2(a,[b,c,a,a,a,d,e]). -> yes 2solutions?
+% – search2(X,[b,c,a,a,d,d,e]). -> yes 2 solutions, for a and d
+% – search2(a,L). -> yes multiple solutions
+% – search2(a,[_,_,a,_,a,_]). ->  yes multiple
